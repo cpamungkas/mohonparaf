ee8e635486e6980d45478761b9136a8aa5a2e94f
Kang CP <necha10.02@gmail.com>
2021-11-21 23:14:04 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      * @var string
      */
     #public $baseURL = 'http://localhost:8080/';
-    public $baseURL ='https://mohonparaf.herokuapp.com/';
+    public $baseURL ='';
 
     /**
      * --------------------------------------------------------------------------
@@ -37,7 +37,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
